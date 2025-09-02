62ab6c5453430b1025af5db2f47760081264bed3
- Azul - <129752425+azulmino@users.noreply.github.com>
2025-09-02 19:39:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -104,14 +104,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isCommand()) return;
 
     if (interaction.commandName === "gamepass") {
-        // 👇 Cambiá "Staff" por el nombre EXACTO del rol
-            const staffRole = interaction.guild.roles.cache.find(r => r.name === "Staff");
-    
-            if (!interaction.member.roles.cache.has(staffRole.id)) {
-                return interaction.reply({
-                    content: "🚫 Solo el **Staff** puede usar este comando.",
-                    ephemeral: true // Solo el usuario ve este mensaje
-                });
         const robux = interaction.options.getInteger("robux");
         let total;
 
